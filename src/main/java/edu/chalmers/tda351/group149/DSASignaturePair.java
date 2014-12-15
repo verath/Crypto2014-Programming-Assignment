bbaea7678135830ef0ba502af6aecc2d81d00ff7
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2014-12-15 16:39:36 +0100

    Forgot file.

---
+++ src/main/java/edu/chalmers/tda351/group149/DSASignaturePair.java
@@ -0,0 +1,22 @@
+package edu.chalmers.tda351.group149;
+
+import java.math.BigInteger;
+
+public class DSASignaturePair {
+
+	private BigInteger r;
+	private BigInteger s;
+
+	public DSASignaturePair(BigInteger r, BigInteger s) {
+		this.r = r;
+		this.s = s;
+	}
+
+	public BigInteger getR() {
+		return r;
+	}
+
+	public BigInteger getS() {
+		return s;
+	}
+}
